dd41829018a7433465f3a7ff0e4d36178898a7ba
xyntechh <xyntechinfo@gmail.com>
2026-09-06 21:27:13 +0530

    add delete functiion in media

--- Server/routes/AdminRoutes/media.routes.js
+++ Server/routes/AdminRoutes/media.routes.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { getAllMedia } = require("../../controller/AdminController/media.controller");
+const { getAllMedia, deleteMedia } = require("../../controller/AdminController/media.controller");
 const Authmiddleware = require("../../middleware/auth")
 
 
 router.get("/getAllMedia", Authmiddleware, getAllMedia)
-
+router.delete("/deleteMedia", Authmiddleware, deleteMedia)
 
 
 
